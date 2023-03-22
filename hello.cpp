4f41b9fc66f5e8c2cfb3eea784f4b86dd4b75a77
Sterbent <104021908+Ster-bent@users.noreply.github.com>
2023-03-22 04:43:11 +0000

    edited the code

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
+    //hi?
+    int variable = 29;
+    cout << variable << endl;
     cout << "Hello, World!" << endl;
-    cout << "Love is a beautiul thing";
+    cout << "Love is a beautiul thing \n";
+    cout << "egg";
     return 0;
 }
 
