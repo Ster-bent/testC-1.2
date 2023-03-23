f49cb139840c89265300a6207151e3bc4fbc23cb
Sterbent <104021908+Ster-bent@users.noreply.github.com>
2023-03-23 01:34:54 +0000

    stuff

--- hello.cpp
+++ hello.cpp
@@ -3,12 +3,18 @@ using namespace std;
 
 int main()
 {
-    //hi?
-    int variable = 29;
-    cout << variable << endl;
-    cout << "Hello, World!" << endl;
-    cout << "Love is a beautiul thing \n";
+    //user input is shown as 'cin'
+    int a,b;
+    cout << "enter the first number \n";
+    cin >> a;
+    cout << "please enter your second number \n";
+    cin >> b;
+    int sum = a + b;
+    cout << "the sum is:" << sum << endl;
+    // cout is like print in other programming languages
     cout << "egg";
+    //auto before a variable will make the variable detect whether or not it is a interger string, or float.
+    auto d = 4.76;
     return 0;
 }
 
