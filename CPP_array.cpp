5a61ff8539355d288bab009f6cbeefe7568192b4
Sterbent <104021908+Ster-bent@users.noreply.github.com>
2023-03-28 18:08:46 +0000

    Test on string arrays in CPP_array.cpp

--- CPP_array.cpp
+++ CPP_array.cpp
@@ -6,6 +6,6 @@ int main()
     int x;
     cout << "enter a number from 0 to 4\n";
     cin >> x;
-    int array[] = {8, 23, 69, 420, 140};
+    string array[] = {"I", "Hate", "Jigs", "Love", "You"};
     cout << array[x] << endl;
 }
