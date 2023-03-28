3419472a8c67031033114fd9fa187df2ac992079
Sterbent <104021908+Ster-bent@users.noreply.github.com>
2023-03-28 17:50:00 +0000

    added user input to CPP_array.cpp

--- CPP_array.cpp
+++ CPP_array.cpp
@@ -3,6 +3,9 @@ using namespace std;
 
 int main()
 {
+    int x;
+    cout << "enter a number from 0 to 4\n";
+    cin >> x;
     int array[] = {8, 23, 69, 420, 140};
-    cout << array[3] << endl;
+    cout << array[x] << endl;
 }
