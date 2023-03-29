c61a7087b9673884dbd050568fda120f53503468
Sterbent <104021908+Ster-bent@users.noreply.github.com>
2023-03-29 17:52:20 +0000

    if statemnt added

--- arrayaccuracy.cpp
+++ arrayaccuracy.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main(){
-    int arr1[] =
+    int x;
+    cout << "enter a number\n";
+    cin >> x;
+    int arr1[] = {1, 1, 1, 1, 1, 1, 1, 1, 1};
+    int arr2[] = {2, 2, 2, 2, 2, 2};
+    int arr3[] = {3, 3, 3, 3, 3, 3, 3};
+    int arr4[] = {4, 4, 4, 4, 4, 4, 4, 5};
+    int arr5[] = {5, 5, 5, 5, 5, 5, 5, 5, 5, 5};
+    if (arr1[x] == true){
+        cout << "has value";
+    }
+    else {
+        cout << "has no value";
+    }
 }
