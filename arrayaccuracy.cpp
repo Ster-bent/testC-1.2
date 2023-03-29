220cd6bd3431bfea4369e5dcc588c1ec4397a8f3
Sterbent <104021908+Ster-bent@users.noreply.github.com>
2023-03-29 18:11:10 +0000

    something

--- arrayaccuracy.cpp
+++ arrayaccuracy.cpp
@@ -11,9 +11,22 @@ int main(){
     int arr4[] = {4, 4, 4, 4, 4, 4, 4, 5};
     int arr5[] = {5, 5, 5, 5, 5, 5, 5, 5, 5, 5};
     if (arr1[x] == true){
-        cout << "has value";
+        cout << "The numbers are accuracy percentage\n";
+        cout << (1/5)*100 << endl;
+        if (arr2[x] == true){
+            cout << (2/5)*100 << endl;
+            if (arr3[x] == true){
+                cout << (3/5)*100 << endl;
+                if (arr4[x] == true){
+                    cout << (4/5)*100 << endl;
+                    if (arr5[x] == true){
+                        cout << (5/5)*100 << endl;
+                    }
+                }
+            }
+        }
     }
     else {
-        cout << "has no value";
+        cout << "Error";
     }
 }
