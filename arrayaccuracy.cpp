9a1c00300b5aabc60ea21a322477da6496e92df9
Sterbent <104021908+Ster-bent@users.noreply.github.com>
2023-04-04 18:26:42 +0000

    `jewa

--- arrayaccuracy.cpp
+++ arrayaccuracy.cpp
@@ -6,6 +6,11 @@ int main(){
     cout << "enter a number\n";
     cin >> x;
     //add float operations for actual percents
+    float a = (1/5)*100;
+    float b = (2/5)*100;
+    float c = (3/5)*100;
+    float d = (4/5)*100;
+    float e = (5/5)*100;
     int arr1[] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
     int arr2[] = {0, 1, 2, 3, 4, 5};
     int arr3[] = {0, 1, 2, 3, 4, 5, 6};
@@ -13,15 +18,15 @@ int main(){
     int arr5[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     if (x == arr1[x]){
         cout << "The numbers are accuracy percentage\n";
-        cout << (1/5)*100 << endl;
+        cout << a << endl;
         if (x == arr2[x]){
-            cout << (2/5)*100 << endl;
+            cout << b << endl;
             if (x == arr3[x]){
-                cout << (3/5)*100 << endl;
+                cout << c << endl;
                 if (x == arr4[x]){
-                    cout << (4/5)*100 << endl;
+                    cout << d << endl;
                     if (x == arr5[x]){
-                        cout << (5/5)*100 << endl;
+                        cout << e << endl;
                     }
                 }
             }
