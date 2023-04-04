42a9e3fd9b009ec9605e925a6517a2d04cc53eee
Sterbent <104021908+Ster-bent@users.noreply.github.com>
2023-04-04 19:53:26 +0000

    add float

--- arrayaccuracy.cpp
+++ arrayaccuracy.cpp
@@ -6,11 +6,11 @@ int main(){
     cout << "enter a number\n";
     cin >> x;
     //add float operations for actual percents
-    float a = (1/5)*100;
-    float b = (2/5)*100;
-    float c = (3/5)*100;
-    float d = (4/5)*100;
-    float e = (5/5)*100;
+    float a = (1.0/5.0)*100;
+    float b = (2.0/5.0)*100;
+    float c = (3.0/5.0)*100;
+    float d = (4.0/5.0)*100;
+    float e = (5.0/5.0)*100;
     int arr1[] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
     int arr2[] = {0, 1, 2, 3, 4, 5};
     int arr3[] = {0, 1, 2, 3, 4, 5, 6};
@@ -18,15 +18,15 @@ int main(){
     int arr5[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     if (x == arr1[x]){
         cout << "The numbers are accuracy percentage\n";
-        cout << a << endl;
+        cout << a<<"%"<< endl;
         if (x == arr2[x]){
-            cout << b << endl;
+            cout << b<<"%"  << endl;
             if (x == arr3[x]){
-                cout << c << endl;
+                cout << c<<"%" << endl;
                 if (x == arr4[x]){
-                    cout << d << endl;
+                    cout << d<<"%" << endl;
                     if (x == arr5[x]){
-                        cout << e << endl;
+                        cout << e<<"%" << endl;
                     }
                 }
             }
